d1505b9aa2adeb17ce433956b14be6e5221d8b72
zhd5120153951 <Zenghedong@outlook.com>
2021-12-23 15:01:24 +0800

    修改了CBot.h的头文件说明

--- Tanker/Tanker/CBot.h
+++ Tanker/Tanker/CBot.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "CTankEntry.h"
+//这是通过Git Bash中的mingw-64模拟Linux下的vim文本编辑器编写的,完全没有用vs2017。
+//这个很方便是因为编写完成后可以直接push，但是无法调试，这个可以在Linux下，配置gcc环境来实现。
 class CBot :
 	public CTankEntry
 {
@@ -8,4 +10,9 @@ public:
 	CBot(float x, float y, PCTSTR szImgName);
 	virtual ~CBot();
 };
+
+
+
+
+
 
